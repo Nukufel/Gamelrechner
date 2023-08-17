58223e3a8cb13588c83f856ba57695fa0a0cf08e
Nukufel <niklas.vogel@bluewin.ch>
2023-08-17 08:20:15 +0200

    working

--- gamelrechner/urls.py
+++ gamelrechner/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.form, name='form'),
-    path('result/', views.result, name='result'),
+    path('<str:gender>/', views.form, name='form'),
+    path('<str:gender>/result/', views.result, name='result'),
+    path('', views.start, name='start')
+
 ]
--- gamelrechner/views.py
+++ gamelrechner/views.py
@@ -1,39 +1,48 @@
 from django.shortcuts import render
 
 # Create your views here.
-def form(request):
-    return render(request, 'form.html')
 
-def result(request):
+def start(request):
+    return render(request, 'start.html')
+
+def form(request, gender):
+    return render(request, 'form.html', {'gender': gender})
+
+def result(request, gender):
     punktzahl = 0
 
     height = int(request.POST['height'])
     weight = int(request.POST['weight'])
     iq = int(request.POST['iq'])
     hair = int(request.POST['hair'])
-    beard = int(request.POST['beard'])
+    beard = int(request.POST.get('beard', 0))
     age = int(request.POST['age'])
 
     print(getPzAge(age))
-    #punktzahl += getPzAge(age)
-    
-    print(getPzHeight(height))
-    punktzahl += getPzHeight(height)*2
+    punktzahl += getPzAge(age)
 
-    print(getPzBmi(height, weight))
-    punktzahl += getPzBmi(height, weight)*2
+    print(getPzHeight(height, gender))
+    punktzahl += getPzHeight(height, gender)
 
-    print(getPzBfp(request.POST['radios']))
-    punktzahl += getPzBfp(request.POST['radios'])*2
+    print("doggy")
+    print(getPzBmi(height, weight, gender))
+    punktzahl += getPzBmi(height, weight, gender)
+
+    if gender != 'female':
+        print(getPzBfp(request.POST['radios']))
+        punktzahl += getPzBfp(request.POST['radios'])
+
+        print(getPzBeard(beard))
+        punktzahl += getPzBeard(beard)
+
+
 
     print(getPzIq(iq))
-    punktzahl += getPzIq(iq)*2
+    punktzahl += getPzIq(iq)
 
     print(getPzHair(hair))
     punktzahl += getPzHair(hair)
 
-    print(getPzBeard(beard))
-    punktzahl += getPzBeard(beard)
 
 
 
@@ -43,72 +52,106 @@ def result(request):
 
 def getPzAge(age):
     if age > 60:
-        return 1
+        return 2
     elif age > 50:
-        return 2
+        return 4
     elif age > 40:
-        return 3
+        return 6
     elif age > 30 or age < 18:
-        return 4
+        return 8
     elif 18 <= age <= 30:
-        return 5
+        return 10
 
 
-def getPzHeight(height):
-    if height <= 154 or height >= 225:
-        return 1
-    elif height <= 164 or height >= 215:
-        return 2
-    elif height <= 174 or height >= 205:
-        return 3
-    elif height <= 184 or height >= 195:
-        return 4
-    elif height <= 194:
-        return 5
+def getPzHeight(height, gender):
+    if gender == 'male':
+        if height <= 154 or height >= 225:
+            return 2
+        elif height <= 164 or height >= 215:
+            return 4
+        elif height <= 174 or height >= 205:
+            return 6
+        elif height <= 184 or height >= 195:
+            return 8
+        elif height <= 194:
+            return 10
+    else:
+        if height <= 129 or height> 200:
+            return 2
+        elif height <= 139 or height >= 190:
+            return 4
+        elif height <= 149 or height >= 180:
+            return 6
+        elif height <= 159 or height >= 170:
+            return 8
+        elif height <= 169:
+            return 10
+
     
-def getPzBmi(height, weight):
+def getPzBmi(height, weight, gender):
     bmi = weight / (height/100) **2
-    if 40 <= bmi:
-        return 1
-    elif bmi < 18.5 or 35 < bmi < 40:
-        return 2
-    elif 18.5 <= bmi < 25:
-        return 4
-    elif 25 <= bmi <= 35:
-        return 5
+
+    if gender == 'male':
+        if 40 <= bmi:
+            return 1
+        elif bmi < 18.5 or 35 < bmi < 40:
+            return 5
+        elif 18.5 <= bmi < 25:
+            return 8
+        elif 25 <= bmi <= 35:
+            return 10
+    else:
+        if 40 <= bmi:
+            return 1
+        elif 30 <= bmi or bmi < 18.5:
+            return 5
+        elif 25 <= bmi:
+            return 8
+        elif 18 <= bmi:
+            return 10
     
 def getPzBfp(bpf):
     if bpf == '5':
-        return 1
-    elif bpf == '4':
         return 2
-    elif bpf == '1' or bpf == '3':
-        return 4
-    elif bpf == '2':
+    elif bpf == '4':
         return 5
+    elif bpf == '1' or bpf == '3':
+        return 8
+    elif bpf == '2':
+        return 10
     
 def getPzIq(iq):
     if iq < 70:
         return 0
     elif iq < 85:
-        return 1
-    elif iq < 100:
         return 2
+    elif iq < 100:
+        return 3
     elif iq < 115:
-        return 3
+        return 5
     elif iq < 130:
-        return 4
+        return 8
     elif 130 <= iq:
-        return 5
+        return 10
     
-def getPzHair(hair):
-    if hair == 1:
-        return 5
-    elif hair == 2:
-        return 3
-    elif hair == 3:
-        return 1
+def getPzHair(hair, gender):
+    if gender == 'male': 
+        if hair == 1:
+            return 5
+        elif hair == 2:
+            return 3
+        elif hair == 3:
+            return 1
+    else:
+        if hair == 1:
+            return 5
+        elif hair == 2:
+            return 3
+        elif hair == 3:
+            return 1
 
 def getPzBeard(beard):
     if beard == 1:
         return 3
+    else:
+        return 0
